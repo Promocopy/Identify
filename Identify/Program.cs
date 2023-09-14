1678d89d8a44ffde901106fc22956ea1411f2f7a
promocopy <tobijosh4@gmail.com>
2023-09-14 21:26:37 +0100

    upgrade

--- Identify/Program.cs
+++ Identify/Program.cs
@@ -1,15 +1,26 @@
 using Identify.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+
 // Add services to the container.
-builder.Services.AddDbContext<PersonContext>(option =>
- option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), sqlServerOptionsAction: sqloption =>
- {
-     sqloption.EnableRetryOnFailure();
- })
-);
+if (connectionString == null)
+{
+    builder.Services.AddDbContext<PersonContext>(option =>
+     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), sqlServerOptionsAction: sqloption =>
+     {
+         sqloption.EnableRetryOnFailure();
+     })
+    );
+}
+else
+{
+    builder.Services.AddDbContext<PersonContext>(options =>
+        options.UseNpgsql(connectionString));
+}
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
